15ace8b6439c004a2916ac24b4604f5f42a3ea81
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-19 13:02:23 -0500

    Update prepare.py

--- data/Chat/prepare.py
+++ data/Chat/prepare.py
@@ -3,10 +3,30 @@ import requests
 import tiktoken
 import numpy as np
 
-# download the tiny shakespeare dataset
+import os
+import requests
+import glob
+
+#concatenates all inputs
+def concatenate_text_files(file_paths):
+  with open("concatenated.txt", "w") as outfile:
+    for file_path in file_paths:
+      response = requests.get(file_path)
+      text = response.text
+      outfile.write(text)
+
+  return os.path.abspath("concatenated.txt")
+
+#save file in colab
+file_paths = ["https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input1.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input2.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input3.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input4.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input5.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input6.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input7.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input8.txt"]
+concatenated_file_path = concatenate_text_files(file_paths)
+
+print(f"The concatenated file is saved at {concatenated_file_path}.")
+
+# get input file
 input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
 if not os.path.exists(input_file_path):
-    data_url = 'https://drive.google.com/uc?export=download&id=1qEFoGmUwi04GuPRRr2ubPpGvjWC_gfUJ'
+    data_url = concatenated_file_path;
     with open(input_file_path, 'w') as f:
         f.write(requests.get(data_url).text)
 
