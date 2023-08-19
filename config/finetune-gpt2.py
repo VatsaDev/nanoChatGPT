26ae6d62de29a5d538644f85ac8da79d0c6abf67
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-19 14:48:06 -0500

    Update finetune-gpt2.py

--- config/finetune-gpt2.py
+++ config/finetune-gpt2.py
@@ -18,8 +18,8 @@ always_save_checkpoint = False
 # shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
 batch_size = 1
 gradient_accumulation_steps = 32
-max_iters = 20
+max_iters = 50
 
 # finetune at constant LR
-learning_rate = 3e-5
+learning_rate = 2e-5
 decay_lr = False
