c8536ea04576161bb1bc4b3b4ec1eda013867f82
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-19 00:12:58 -0500

    Update finetune-gpt2.py

--- config/finetune-gpt2.py
+++ config/finetune-gpt2.py
@@ -8,7 +8,7 @@ wandb_project = 'gpt2finetune'
 wandb_run_name = 'ft-' + str(time.time())
 
 dataset = 'Chat'
-init_from = 'gpt2' # this is the smallest GPT-2 model, good for google colab
+init_from = 'gpt2-medium' # this is the smallest GPT-2 model, good for google colab
 
 # only save checkpoints if the validation loss improves
 always_save_checkpoint = False
