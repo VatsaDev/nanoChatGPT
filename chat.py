d894c87ade40a9ef594952f7a0c0fbcd75a79a5a
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-22 09:36:47 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -18,6 +18,7 @@ top_k = 150 # retain only the top_k most likely tokens, clamp others to have 0 p
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
 compile = False # use PyTorch 2.0 to compile the model to be faster
+exec(open('configurator.py').read()) # overrides from command line, only for out_dir location, if you store the ckpt.pt elsewhere, like gdrive, to escape finetuning everytime you run the colab
 # -----------------------------------------------------------------------------
 
 torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
