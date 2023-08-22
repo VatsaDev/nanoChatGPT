c308f45e0cbdbb05c56836e6205416c27bdbd8b8
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-22 09:33:48 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -83,4 +83,5 @@ while True:
             for k in range(num_samples):
                 y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
                 text = decode(y[0].tolist())
+                text.replace(start_ids,'')
                 print(text)
