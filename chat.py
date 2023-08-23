afe05956a70f903c92ab56b77ee6c6601990de26
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-22 20:45:06 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -82,6 +82,6 @@ while True:
     context=context+start
     
     out = respond(context, num_samples)
-    context=context+'<bot>'+context+'<endOfText>'
+    context=context+'<bot>'+out+'<endOfText>'
     print('Bot: '+ out)
     print('context: '+context)
