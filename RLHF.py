af056d9573b2c353d2bd31a69b05fd4b91652dce
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-24 10:54:40 -0500

    Create RLHF.py

---
+++ RLHF.py
@@ -0,0 +1,6 @@
+# send contents to makersuite api for improvement
+# -----------------------------------------------------------------------------
+out_file = "improve.txt" # file where improved text is written to
+
+exec(open('configurator.py').read()) # cmd overwrite
+# -----------------------------------------------------------------------------
