ea18dcae1cb313bf2c63afdb034bc49d74fda50c
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-24 12:01:36 -0500

    Update improve.py

--- improve.py
+++ improve.py
@@ -3,19 +3,15 @@
 import pprint
 import google.generativeai as palm
 
-# -----------------------------------------------------------------------------
 out_file = "improve.txt" # file where improved text is written to
-user_api_key = "#####" # enter your makersuite key in cmd line
-exec(open('configurator.py').read()) # cmd overwrite
-# -----------------------------------------------------------------------------
 
-palm.configure(api_key=user_api_key)
+def improve(text_improve, user_api_key):
+  palm.configure(api_key=user_api_key)
+  
+  #get text bison
+  models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+  model = models[0].name
 
-#get text bison
-models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
-model = models[0].name
-
-def improve(text_improve):
   prompt = "Return Chat conversation improved ```"+text_improve+"```"
 
   completion = palm.generate_text(
@@ -26,4 +22,4 @@ def improve(text_improve):
       max_output_tokens=800,
   )
   
-  print(completion.result)
+  return completion.result
