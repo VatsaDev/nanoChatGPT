61c5a820ea194ed5f55510b3d0e29addb20aa9b8
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-24 11:52:22 -0500

    Update improve.py

--- improve.py
+++ improve.py
@@ -16,7 +16,7 @@ models = [m for m in palm.list_models() if 'generateText' in m.supported_generat
 model = models[0].name
 
 def improve(text_improve):
-  prompt = text_improve
+  prompt = "Return Chat conversation improved ```"+text_improve+"```"
 
   completion = palm.generate_text(
       model=model,
