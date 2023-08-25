834d407c1afa164748375dc9369a2bc3f8527052
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-24 21:33:28 -0500

    Update finetune-gpt2.py

--- config/finetune-gpt2.py
+++ config/finetune-gpt2.py
@@ -13,9 +13,6 @@ init_from = 'gpt2-medium' # this is the smallest GPT-2 model, good for google co
 # only save checkpoints if the validation loss improves
 always_save_checkpoint = False
 
-# the number of examples per iter:
-# 1 batch_size * 32 grad_accum * 1024 tokens = 32,768 tokens/iter
-# shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
 batch_size = 4
 gradient_accumulation_steps = 32
 max_iters = 50
