4c72e1895c697453c5873184c7910c5c23a8f18e
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-25 13:53:04 -0500

    Update prepare.py

--- data/Chat/prepare.py
+++ data/Chat/prepare.py
@@ -7,31 +7,19 @@ import os
 import requests
 import glob
 
-#concatenates all inputs
-def concatenate_text_files(file_paths):
-  with open("concatenated.txt", "w") as outfile:
-    for file_path in file_paths:
-      response = requests.get(file_path)
-      text = response.text
-      outfile.write(text)
+def download_file(url):
+  response = requests.get(url)
+  if response.status_code == 200:
+    with open('dataset.txt', 'wb') as f:
+      f.write(response.content)
+      print("downloaded dataset, tokenizing")
+  else:
+    print('Error downloading file:', response.status_code)
 
-  return os.path.abspath("concatenated.txt")
-
-#save file in colab
-file_paths = ["https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input1.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input2.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input3.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input4.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input5.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input6.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input7.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input8.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input9.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input10.txt"
-             "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input11.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input12.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input13.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input14.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input15.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input16.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input17.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input18.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input19.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input20.txt"
-             "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input21.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input22.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input23.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input24.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input25.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input26.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input27.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input28.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input29.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input30.txt"
-             "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input31.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input32.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input33.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input34.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input35.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input36.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input37.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input38.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input39.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input40.txt",
-             "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input41.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input42.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input43.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input44.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input45.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input46.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input47.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input48.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input49.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input50.txt",
-             "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input51.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input52.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input53.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input54.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input55.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input56.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input57.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input58.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input59.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input60.txt",
-             "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input61.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input62.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input63.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input64.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input65.txt", "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input66.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input67.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input68.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input69.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input70.txt",
-             "https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input71.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input72.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input73.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input74.txt","https://raw.githubusercontent.com/VatsaDev/nanoChatGPT/main/data/Chat/input75.txt",]
-concatenated_file_path = concatenate_text_files(file_paths)
-
-print(f"The concatenated file is saved at {concatenated_file_path}.")
+download_file('https://huggingface.co/VatsaDev/ChatGpt-nano/resolve/main/Dataset.txt')
 
 # get input file
-input_file_path = './concatenated.txt';
+input_file_path = './dataset.txt';
 
 with open(input_file_path, 'r') as f:
     data = f.read()
@@ -42,6 +30,7 @@ val_data = data[int(n*0.9):]
 # encode with tiktoken gpt2 bpe
 enc = tiktoken.get_encoding("gpt2")
 train_ids = enc.encode_ordinary(train_data)
+print(train_ids[0])
 val_ids = enc.encode_ordinary(val_data)
 print(f"train has {len(train_ids):,} tokens")
 print(f"val has {len(val_ids):,} tokens")
