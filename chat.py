5d7923b3db74fba7baa0a0515935e8d0072b7bfb
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-25 20:08:42 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -30,6 +30,14 @@ device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.aut
 ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
 ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
+def download_ckpt(url):
+  response = requests.get(url)
+  if response.status_code == 200:
+    with open('ckpt.pt', 'wb') as f:
+      f.write(response.content)
+  else:
+    print('Error downloading file:', response.status_code)
+
 # gets model
 # init from a model saved in a specific directory
 if init_from == 'huggingface':
