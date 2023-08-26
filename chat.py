5d16ee4b6a911283e3a8aaff1bd2cdd34dc30dcd
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-25 20:50:56 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -19,9 +19,10 @@ temperature = 0.9
 top_k = 150 # retain only the top_k most likely tokens, clamp others to have 0 probability
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
-compile = False # use PyTorch 2.0 to compile the model to be faster
+compile = True # use PyTorch 2.0 to compile the model to be faster
 improve_msuite = False # true if you want to use makersuite improve
 your_api_key = "#####" # enter your makersuite key in cmd line
+context="<human>Hello, how are you?<endOfText><bot>Thanks, Im good, what about you?<endOfText><human>Im great thanks, My names James, and I'm from the UK, wbu?<endOfText><bot>Hi James, I'm Conner, and im from america. <endOftext>" # a little context for better chat responses
 exec(open('configurator.py').read()) # overrides from command line, only for out_dir location, if you store the ckpt.pt elsewhere, like gdrive, to escape finetuning everytime you run the colab
 # -----------------------------------------------------------------------------
 
@@ -112,7 +113,6 @@ def respond(input, samples): # generation function
                 return output_text
 
 # chat loop
-context=''
 while True:
     # get input from user
     start_input = input('User: ')
